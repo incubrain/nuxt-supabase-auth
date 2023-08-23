79c4d1f66a7053a1c62e54e8740000b228036b30
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-23 09:52:38 +0530

    chore(server): use emails to validate admin

--- server/utils/isAdminUser.ts
+++ server/utils/isAdminUser.ts
@@ -1,9 +1,11 @@
 import type { H3Event } from 'h3'
 
+const adminEmails = ['drewmacgibbon@gmail.com']
+
 export default async function isAdminUser(event: H3Event): Promise<boolean> {
   const user = await serverSupabaseUser(event)
   if (!user) return false
-  console.log('user', user.id, user.id !== '4640b591-9c7b-45d1-a64d-e133442f48bd')
-  if (user.id !== '4640b591-9c7b-45d1-a64d-e133442f48bd') return false
+  console.log('user', user.email, !adminEmails.includes(user.email!))
+  if (!adminEmails.includes(user.email!)) return false
   return true
 }
